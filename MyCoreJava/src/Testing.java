14964d8142cd468721137e3e56fcca8741a0eb33
vilasparab <parab.vilas@gmail.com>
2022-05-10 15:49:24 +0530

    we made bean student

--- MyCoreJava/src/Testing.java
+++ MyCoreJava/src/Testing.java
@@ -1,4 +1,12 @@
 
+class Student
+{
+	private int code;
+	
+	  public Student(int c)
+	  {  code=c;
+	  }
+}
 public class Testing {
   public static void main(String[] args) {
 	System.out.println("Testing work");
